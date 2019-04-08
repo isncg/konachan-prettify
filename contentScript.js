1ed7cf2993e0328086dc8d74f74f0eed08b1a85d
EthanNote <hanyishen32@foxmail.com>
2019-04-08 18:40:28 +0800

    first commit

---
+++ contentScript.js
@@ -0,0 +1,22 @@
+(function(){
+    document.getElementsByClassName('sidebar')[0].style.marginRight="0"
+    document.getElementsByClassName('content')[0].style.width="85%"
+
+    console.log('Hello')
+    let ul = document.getElementById('post-list-posts')
+    let lis = ul.children
+    for(let i =0;i<lis.length; i++){
+        let li = lis[i]
+        li.children[1].remove()
+        li.style.width="370px"
+        let img = li.getElementsByTagName('img')[0]
+        let frame = li.getElementsByClassName('inner')[0]
+        frame.style.width="360px"
+        frame.style.height="360px"
+        var zoom = 360.0/img.width
+        img.width*=zoom
+        img.height*=zoom
+        // img.width*=2 //150*2
+        // img.height*=2 
+    }
+})()
