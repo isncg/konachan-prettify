26ae99f71a5e93f7c1a655094a3bf1072a23ba56
EthanNote <hanyishen32@foxmail.com>
2019-04-11 16:43:45 +0800

    yande support

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,6 @@
-(function(){
-
-    console.log("Konachan Clean View")
+console.log("konachan-big-preview")
+console.log("Star this project: https://github.com/EthanNote/konachan-big-preview")
+if(window.location.host==="konachan.net" || window.location.host==="konachan.com"){
 
     let ad = document.getElementsByTagName('iframe')
     while(ad && ad.length>0){
@@ -16,7 +16,6 @@
     document.getElementById('paginator').style.marginRight= "20%"
     let ul = document.getElementById('post-list-posts')
     let lis = ul.children
-    console.log(lis)
     for(let i =0;i<lis.length; i++){
         let li = lis[i]
         while(li.children && li.children.length>1){
@@ -30,7 +29,31 @@
         let zoom = 360.0/img.width
         img.width*=zoom
         img.height*=zoom
-        // img.width*=2 //150*2
-        // img.height*=2 
     }
-})()
+}
+
+else if(window.location.host==="yande.re" ){
+   
+
+    document.getElementsByClassName('sidebar')[0].style.marginRight="0"
+    document.getElementsByClassName('sidebar')[0].style.width="15%"
+    document.getElementsByClassName('content')[0].style.width="85%"
+    document.getElementById('paginator').style.marginRight= "20%"
+    let ul = document.getElementById('post-list-posts')
+    let lis = ul.children
+    for(let i =0;i<lis.length; i++){
+        let li = lis[i]
+        while(li.children && li.children.length>1){
+            li.children[1].remove()
+        }
+        li.style.width="370px"
+        let img = li.getElementsByTagName('img')[0]
+        let frame = li.getElementsByClassName('inner')[0]
+        frame.style.width="360px"
+        frame.style.height="480px"
+        frame.style.backgroundColor = "#252525"
+        let zoom = 360.0/img.width
+        img.width*=zoom
+        img.height*=zoom
+    }
+}
